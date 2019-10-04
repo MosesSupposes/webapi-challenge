989578151b17efc018de1b823d888c909c4e2fcf
Moses A. Samuel <mosamuel98@gmail.com>
2019-10-04 09:04:15 -0700

    make respondWithError helper more robust

--- data/helpers/respondWithError.js
+++ data/helpers/respondWithError.js
@@ -1,5 +1,15 @@
-function respondWithError(res={}, statusCode=500, errObj={}) {
-    res.status(statusCode).json(errObj)
+function respondWithError(res={}, statusCode=500, err={}) {
+    const defaultErrorMsg = "There was trouble processing your request"
+    if (typeof err === 'object' || typeof err === 'string') {
+        (typeof err === 'object') &&
+            (!Ojbect.keys(err).length )
+                ? res.status(statusCode).json(errObj)
+                : res.status(statusCode).json({error: defaultErrorMsg}) 
+        (typeof err === 'string') &&
+            res.status(statusCode).json({error: err})
+    } else {
+        res.status(statusCode).json({error: defaultErrorMsg})
+    }
 }
 
 module.exports = respondWithError
