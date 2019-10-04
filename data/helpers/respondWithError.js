6a9980c1410a6097931a3dd1d2bcb630bcb27895
Moses A. Samuel <mosamuel98@gmail.com>
2019-10-04 09:58:53 -0700

    fix typo

--- data/helpers/respondWithError.js
+++ data/helpers/respondWithError.js
@@ -2,7 +2,7 @@ function respondWithError(res={}, statusCode=500, err={}) {
     const defaultErrorMsg = "There was trouble processing your request"
     if (typeof err === 'object' || typeof err === 'string') {
         (typeof err === 'object') &&
-            (!Ojbect.keys(err).length )
+            (!Object.keys(err).length )
                 ? res.status(statusCode).json({error: defaultErrorMsg}) 
                 : res.status(statusCode).json(err)
         (typeof err === 'string') &&
