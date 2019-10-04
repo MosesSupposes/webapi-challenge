7d31bcd4da7b695bc9257193a343df4c5c5df912
Moses A. Samuel <mosamuel98@gmail.com>
2019-10-04 10:28:23 -0700

    fix bug on delete endpoint on project router

--- routes/projects.js
+++ routes/projects.js
@@ -39,7 +39,7 @@ router.put('/:project_id', async function(req, res) {
     else res.status(200).json(project)
 })
 
-router.delete('/project_id', async function (req, res) {
+router.delete('/:project_id', async function (req, res) {
     const [err] = await withCatch(projectModel.remove(req.params.project_id))
     if (err) respondWithError(res, 500, "We ran into an error when attempting to delete the specified project.")
     else res.status(200).json({message: "Successfully deleted the project with the id of " + req.params.id})
